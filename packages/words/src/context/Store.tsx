5a62b265528a2171833369154ee9fe31c0df360c
Алексей Иванов <alexey.ivanov@travelline.ru>
2023-03-25 08:54:54 +0300

    Todo

--- packages/words/src/context/Store.tsx
+++ packages/words/src/context/Store.tsx
@@ -1,13 +1,33 @@
 import { createContext, useContext, useState } from "react";
 import { Functions } from "words-ui";
+import type { Todo } from "../data/Todo";
 
-const defaultData = { count: 0, userName: "" };
-// eslint-disable-next-line react/hook-use-state
-const useStoredData = () => useState(defaultData);
+const defaultData: Todo[] = [];
+
+const useStoredData = () => {
+  const [data, setData] = useState(defaultData);
+  const addTodo = (newTodo: Todo) => setData([...data, newTodo]);
+  const deleteTodo = (id: number) => setData(data.filter(item => item.id !== id));
+
+  const toggleTodo = (id: number) =>
+    setData(data.map(item => (item.id === id ? { id, isDone: !item.isDone, title: item.title } : item)));
+
+  return {
+    addTodo,
+    deleteTodo,
+    todoList: data,
+    toggleTodo,
+  };
+};
 
 type StoreState = ReturnType<typeof useStoredData>;
 
-const StoreContext = createContext<StoreState>([defaultData, Functions.empty]);
+const StoreContext = createContext<StoreState>({
+  addTodo: Functions.empty,
+  deleteTodo: Functions.empty,
+  todoList: [],
+  toggleTodo: Functions.empty,
+});
 
 type StoreProps = {
   children: React.ReactNode;
